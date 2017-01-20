12bbf4b25257be4c583a45eb77461dd3d06be4a6
wrist <stoicheia1986@gmail.com>
2017-01-20 20:41:26 +0900

    changed jupyter notebook using port

--- docker/run_notebook.sh
+++ docker/run_notebook.sh
@@ -1,1 +1,1 @@
-docker run -i -t -p 8888:8888 wrist/pydata-osaka3 /bin/bash -c "jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser"
+docker run -i -t -p 18888:18888 wrist/pydata-osaka3 /bin/bash -c "jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=18888 --no-browser"

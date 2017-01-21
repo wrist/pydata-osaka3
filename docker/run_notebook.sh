77f126aa5e793ffa7368c75b8b13e5b89af73783
wrist <stoicheia1986@gmail.com>
2017-01-21 10:22:16 +0900

    update run notebook script

--- docker/run_notebook.sh
+++ docker/run_notebook.sh
@@ -1,1 +1,1 @@
-docker run -i -t -p 18888:18888 wrist/pydata-osaka3 /bin/bash -c "jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=18888 --no-browser"
+docker run -i -t -p 18888:18888 wrist/pydata-osaka3 /bin/bash -c "export LD_LIBRARY_PATH=/usr/local/lib; jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=18888 --no-browser"
